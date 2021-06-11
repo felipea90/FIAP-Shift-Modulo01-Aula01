c22baefc6d6d3a309c0d31f36b057136a6ed9eb9
Felipe Amaral <felipe.abcosta90@gmail.com>
2021-06-10 21:47:09 -0300

    Calculadora condicional de operador

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -26,13 +26,31 @@ public class Calculadora {
 		div = valor1 / valor2;
 		multi = valor1 * valor2;
 		
-		System.out.println("A Soma dos valores é: " + soma);
+		System.out.println("Escolha a operação Matemática:");
 		
-		System.out.println("A Subtração dos valores é: " + sub);
+		System.out.println("1 = Soma\n2 = Divisão\n3 = Subtração\n4 = Multiplicação:");
 		
-		System.out.println("A Divisão dos valores é: " + div);
+		escolha = leitor.nextInt();
 		
-		System.out.println("A Multiplicação dos valores é: " + multi);
+		if (escolha == 1) {
+			
+			System.out.println("A Soma dos valores é: " + soma);
+		}
+		
+		if (escolha == 2) {
+			
+			System.out.println("A Divisão dos valores é: " + div);
+		}
+		
+		if (escolha == 3) {
+			
+			System.out.println("A Subtração dos valores é: " + sub);
+		}
+		
+		if (escolha == 4) {
+			
+			System.out.println("A Multiplicação dos valores é: " + multi);
+		}
 		
 		
 		
